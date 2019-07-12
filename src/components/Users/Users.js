7a4d72392b9711a3a67b545e80ea2565454e2eb9
muchmallow <31728584+muchmallow@users.noreply.github.com>
2019-07-12 09:12:53 +0300

    axios installed
    having an error (Unhandled Rejection (TypeError): action.users is not iterable) in usersReducer.js 37

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,65 +1,71 @@
 import React from "react";
 import classes from "./Users.module.css";
+import * as axios from "axios";
 
 let Users = (props) => {
 
 	if(props.users.length === 0){
-		props.setUsers([
-			{
-				id: 1,
-				photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
-				followed: true,
-				fullName: "Yuriy",
-				status: "Im a boss",
-				location: {city: "Kharkov", country: "Ukraine"}
-			},
-			{
-				id: 2,
-				photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
-				followed: false,
-				fullName: "Vladislav",
-				status: "Lel",
-				location: {city: "Kiev", country: "Ukraine"}},
-			{
-				id: 3,
-				photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
-				followed: true,
-				fullName: "Veronika",
-				status: "Kek",
-				location: {city: "Kiev", country: "Ukraine"}},
-			{
-				id: 4,
-				photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
-				followed: false,
-				fullName: "Dmitriy",
-				status: "Zaebisya pahnet pisya",
-				location: {city: "Kiev", country: "Ukraine"}
-			},
-			{
-				id: 5,
-				photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
-				followed: true,
-				fullName: "Svetlana",
-				status: "A na to ona i pisya wobu pahla zaebisya",
-				location: {city: "Kiev", country: "Ukraine"}
-			},
-			{
-				id: 6,
-				photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
-				followed: false,
-				fullName: "Court",
-				status: "Im a boss",
-				location: {city: "Washington", country: "USA"}
-			},
-			{
-				id: 7,
-				photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
-				followed: true,
-				fullName: "Carley",
-				status: "Zdarova ebat",
-				location: {city: "Massachusetts", country: "USA"}
-			}
-		])
+
+		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+			props.setUsers();
+		});
+
+		// props.setUsers([
+		// 	{
+		// 		id: 1,
+		// 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+		// 		followed: true,
+		// 		fullName: "Yuriy",
+		// 		status: "Im a boss",
+		// 		location: {city: "Kharkov", country: "Ukraine"}
+		// 	},
+		// 	{
+		// 		id: 2,
+		// 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+		// 		followed: false,
+		// 		fullName: "Vladislav",
+		// 		status: "Lel",
+		// 		location: {city: "Kiev", country: "Ukraine"}},
+		// 	{
+		// 		id: 3,
+		// 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+		// 		followed: true,
+		// 		fullName: "Veronika",
+		// 		status: "Kek",
+		// 		location: {city: "Kiev", country: "Ukraine"}},
+		// 	{
+		// 		id: 4,
+		// 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+		// 		followed: false,
+		// 		fullName: "Dmitriy",
+		// 		status: "Zaebisya pahnet pisya",
+		// 		location: {city: "Kiev", country: "Ukraine"}
+		// 	},
+		// 	{
+		// 		id: 5,
+		// 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+		// 		followed: true,
+		// 		fullName: "Svetlana",
+		// 		status: "A na to ona i pisya wobu pahla zaebisya",
+		// 		location: {city: "Kiev", country: "Ukraine"}
+		// 	},
+		// 	{
+		// 		id: 6,
+		// 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+		// 		followed: false,
+		// 		fullName: "Court",
+		// 		status: "Im a boss",
+		// 		location: {city: "Washington", country: "USA"}
+		// 	},
+		// 	{
+		// 		id: 7,
+		// 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+		// 		followed: true,
+		// 		fullName: "Carley",
+		// 		status: "Zdarova ebat",
+		// 		location: {city: "Massachusetts", country: "USA"}
+		// 	}
+		// ])
 	}
 
 	return (
