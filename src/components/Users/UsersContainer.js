e78cfb47d4a47be646f4c35885ce5cea42571469
muchmallow <31728584+muchmallow@users.noreply.github.com>
2019-07-18 20:04:33 +0300

    Refactored Users,js and UsersContainer.js
    workin well

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,7 +1,92 @@
 import React from "react";
-import Users from "./Users";
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/usersReducer";
+import * as axios from "axios";
+import Users from "./Users";
+
+class UsersAPIComponent extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    };
+
+    // props.setUsers([
+    // 	{
+    // 		id: 1,
+    // 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+    // 		followed: true,
+    // 		fullName: "Yuriy",
+    // 		status: "Im a boss",
+    // 		location: {city: "Kharkov", country: "Ukraine"}
+    // 	},
+    // 	{
+    // 		id: 2,
+    // 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+    // 		followed: false,
+    // 		fullName: "Vladislav",
+    // 		status: "Lel",
+    // 		location: {city: "Kiev", country: "Ukraine"}},
+    // 	{
+    // 		id: 3,
+    // 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+    // 		followed: true,
+    // 		fullName: "Veronika",
+    // 		status: "Kek",
+    // 		location: {city: "Kiev", country: "Ukraine"}},
+    // 	{
+    // 		id: 4,
+    // 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+    // 		followed: false,
+    // 		fullName: "Dmitriy",
+    // 		status: "Zaebisya pahnet pisya",
+    // 		location: {city: "Kiev", country: "Ukraine"}
+    // 	},
+    // 	{
+    // 		id: 5,
+    // 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+    // 		followed: true,
+    // 		fullName: "Svetlana",
+    // 		status: "A na to ona i pisya wobu pahla zaebisya",
+    // 		location: {city: "Kiev", country: "Ukraine"}
+    // 	},
+    // 	{
+    // 		id: 6,
+    // 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+    // 		followed: false,
+    // 		fullName: "Court",
+    // 		status: "Im a boss",
+    // 		location: {city: "Washington", country: "USA"}
+    // 	},
+    // 	{
+    // 		id: 7,
+    // 		photoUrl: "https://n1s1.starhit.ru/b8/7d/3e/b87d3e9ee04c16a0a286a6ddb12aa81f/480x497_0_888c4da80d90808bffc8babaa9f22ecd@480x497_0xc0a8399a_9271194921514978916.jpeg",
+    // 		followed: true,
+    // 		fullName: "Carley",
+    // 		status: "Zdarova ebat",
+    // 		location: {city: "Massachusetts", country: "USA"}
+    // 	}
+    // ])
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+        />;
+    }
+}
 
 let mapStateToProps = (state) => {
 	return {
@@ -32,6 +117,6 @@ let mapDispatchToProps = (dispatch) => {
 	}
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
 
 export default UsersContainer;
