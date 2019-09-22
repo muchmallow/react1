7ac0c2c5984b219c3b54cf224f7b48742438e9a1
muchmallow <31728584+muchmallow@users.noreply.github.com>
2019-09-23 02:42:25 +0300

    Updates

---
+++ precache-manifest.f075255e515313eafc79249ad7671ba4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad461c080f02e07e1b90da1bfe72c86",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "2b97a99be51449691956",
+    "url": "/react1/static/css/3.645dbb4b.chunk.css"
+  },
+  {
+    "revision": "ce078532d3e1fa72efd6",
+    "url": "/react1/static/css/4.7dbf4bd7.chunk.css"
+  },
+  {
+    "revision": "4d28a802668df5f2b86d",
+    "url": "/react1/static/css/5.5f81c1ca.chunk.css"
+  },
+  {
+    "revision": "098565235ba524e607a0",
+    "url": "/react1/static/css/main.27df26ed.chunk.css"
+  },
+  {
+    "revision": "654477cc1d0870b2e74d",
+    "url": "/react1/static/js/2.0c753a7a.chunk.js"
+  },
+  {
+    "revision": "2b97a99be51449691956",
+    "url": "/react1/static/js/3.707f4b21.chunk.js"
+  },
+  {
+    "revision": "ce078532d3e1fa72efd6",
+    "url": "/react1/static/js/4.978e5c6f.chunk.js"
+  },
+  {
+    "revision": "4d28a802668df5f2b86d",
+    "url": "/react1/static/js/5.c1cf054c.chunk.js"
+  },
+  {
+    "revision": "098565235ba524e607a0",
+    "url": "/react1/static/js/main.74910ce5.chunk.js"
+  },
+  {
+    "revision": "e152a542c90972587eed",
+    "url": "/react1/static/js/runtime~main.dd1840ce.js"
+  },
+  {
+    "revision": "c5dcdbc6e975423c05a340408c55d27a",
+    "url": "/react1/static/media/preloader.c5dcdbc6.svg"
+  },
+  {
+    "revision": "04867da480750844007755086b31e42a",
+    "url": "/react1/static/media/user_image_male.04867da4.png"
+  }
+]);
